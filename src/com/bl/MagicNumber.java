3a3c9049d49a2254b792012c3b3692f3a1d1dfcb
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-09 10:35:46 +0530

    Added : algoritham practice

---
+++ src/com/bl/MagicNumber.java
@@ -0,0 +1,31 @@
+package com.bl;
+
+import java.util.Scanner;
+
+public class MagicNumber {
+
+    public static void main(String[] args) {
+        int result=0;
+        int smallestNumber = 0 ;
+        int biggestNumber =100;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            int sum = smallestNumber+biggestNumber;
+            int magicNumber = sum/2;
+            System.out.println("1.Number is greater than "+magicNumber);
+            System.out.println("2.Number is lesser than "+magicNumber);
+            System.out.println("3.Number is equal to "+magicNumber);
+            int choice = scanner.nextInt();
+            if (choice==1){
+                smallestNumber=magicNumber;
+            }else if(choice==2){
+                biggestNumber=magicNumber;
+            } else if (choice==3) {
+                result=1;
+                System.out.println("Magic number found.");
+            }else {
+                System.out.println("Enter valid inputs.");
+            }
+        }while (result==0);
+    }
+}
